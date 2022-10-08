9ae8b9b46f58ab0073fff147def9cbdd018c2c29
Volodymyr-Zastavniuk <106408963+Volodymyr-Zastavniuk@users.noreply.github.com>
2022-10-09 02:11:27 +0300

    adds task 1 and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,61 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryContainer: document.querySelector(".gallery"),
+};
+
+let imgInOriginalSize;
+
+refs.galleryContainer.insertAdjacentHTML(
+  "beforeend",
+  createGalleryMarkup(galleryItems)
+);
+
+refs.galleryContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  showImgInOriginalSize(e);
+});
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+       <div class="gallery__item">
+         <a class="gallery__link" href="${original}">
+           <img
+             class="gallery__image"
+             src="${preview}"
+             data-source="${original}"
+             alt="${description}"
+           />
+         </a>
+       </div>`;
+    })
+    .join("");
+}
+
+function showImgInOriginalSize(e) {
+  imgInOriginalSize = basicLightbox.create(
+    `<img src="${e.target.dataset.source}">`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", onEscapeKeyPress);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", onEscapeKeyPress);
+      },
+    }
+  );
+  imgInOriginalSize.show();
+}
+
+function onEscapeKeyPress(e) {
+  if (e.code === "Escape") {
+    imgInOriginalSize.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryContainer: document.querySelector(".gallery"),
+};
+
+refs.galleryContainer.insertAdjacentHTML(
+  "beforeend",
+  createGalleryMarkup(galleryItems)
+);
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+         <a class="gallery__link" href="${original}">
+           <img
+             class="gallery__image"
+             src="${preview}"
+             data-source="${original}"
+             alt="${description}"
+           />
+         </a>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
